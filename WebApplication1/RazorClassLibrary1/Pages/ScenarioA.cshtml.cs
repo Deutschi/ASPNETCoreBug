1a61fbaaf8e73eb14cacc909c0c18c5f0d71be49
Stefan Deutsch <stefan.deutsch@korn.eu>
2020-04-09 11:20:35 +0200

    add sample project

---
+++ WebApplication1/RazorClassLibrary1/Pages/ScenarioA.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorClassLibrary1.Pages
+{
+    public class ScenarioAModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
